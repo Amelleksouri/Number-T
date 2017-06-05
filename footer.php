89976651c664cd687931ecf77d3ace235d270f59
Amelleksouri <amelleksouri.ecolesrp@gmail.com>
2017-06-05 17:07:39 +0200

    premier commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+        <footer>
+
+          <script src="https://use.fontawesome.com/9579265c86.js"></script>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="js/bootstrap.min.js"></script>
+
+
+        </footer>
+
+          </body>
+
+
+
+        </html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta name="description" content="Bar à Thé, maquette graphique réalisé par Christelle Hennette">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet">
+
+    <title>Bar à Thé</title>
+
+  </head>
+  <body>
